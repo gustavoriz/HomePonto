286c3d4964621237b1b97c71a423f95ffcda7cb2
gustavoriz <73759398+gustavoriz@users.noreply.github.com>
2024-06-15 08:15:05 -0300

    Add files via upload

---
+++ HomePontoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePontoApp.swift
+//  HomePonto
+//
+//  Created by Gustavo on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HomePontoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
